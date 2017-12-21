0f9af859238923dd6067a1adf02e4985554e5466
Alison <aacuna136@gmail.com>
2017-12-21 14:58:53 -0800

    First commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-40d28157"
+  instance_type = "t2.micro"
+  tags {
+  Name = "terraform-example"
+  }
+}
