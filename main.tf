3357599f1412b57e99508d59f004203d65cda83d
Alison <aacuna136@gmail.com>
2017-12-27 06:57:03 -0800

    added single web server terraform code

--- main.tf
+++ main.tf
@@ -1,13 +1,38 @@
+terraform {
+  required_version = ">= 0.8"
+}
+
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
   region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-40d28157"
-  instance_type = "t2.micro"
+  ami                    = "ami-40d28157"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags {
-  Name = "terraform-example-one"
+    Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
